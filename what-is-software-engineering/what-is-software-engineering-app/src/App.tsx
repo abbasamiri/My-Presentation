6ce0ebdde098db12e82e56665c9dd3091d1b79b9
Abbas Amiri <abbas.amiri.developer@outlook.com>
2025-01-23 14:01:52 +0100

    Some changes

---
+++ what-is-software-engineering/what-is-software-engineering-app/src/App.tsx
@@ -0,0 +1,15 @@
+import Presentation from "./Presentation.tsx";
+import './index.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+function App() {
+
+    return (
+        <>
+            <Presentation/>
+        </>
+    );
+}
+
+export default App
